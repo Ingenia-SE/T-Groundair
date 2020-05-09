3d5893315ee2691918378b75178fa93163598d44
jruro <jruro7768@gmail.com>
2020-05-09 14:38:15 +0200

    9/5/2020

---
+++ software/GAZEBO/ros_gazebo_v1/build/CMakeFiles/central_station_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/central_station_plugin.dir/src/central_station.cc.o"
+  "devel/lib/libcentral_station_plugin.pdb"
+  "devel/lib/libcentral_station_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/central_station_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
